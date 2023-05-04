d15c78b38744b4716632c73f30f5d40a8cad94ab
Michel Maciel <michel.asm@hotmail.com>
2023-05-04 00:56:02 -0300

    3 arquivos principais adicionados

---
+++ script.js
@@ -0,0 +1,41 @@
+const arrayDasPalavras = ["impressionante.", "muito massa!", "DELICINHA :3", "bom pra buné!", "ostentavel.", "show de bola...", "no grau!", "sobressalente.", "pegando fogo bixo!"];
+const spanDaPalavra = document.querySelector('.span-da-palavra');
+const cursor = document.querySelector('.cursor');
+const piscando = 'piscando'
+let i = 0;
+let j = 0;
+
+function escreverPalavra() {
+  const arrayDasLetras = arrayDasPalavras[i].split("");
+  setTimeout(() => {
+    j++;
+    spanDaPalavra.textContent = arrayDasLetras.slice(0, j).join("");
+    if (j < arrayDasLetras.length) {
+      cursor.classList.remove(piscando);
+      escreverPalavra();
+    } else {
+      cursor.classList.add(piscando);
+      setTimeout(apagarPalavra, 1500);
+    }
+  }, 60);
+}
+
+function apagarPalavra() {
+  const arrayDasLetras = arrayDasPalavras[i].split("");
+  setTimeout(() => {
+    j--;
+    spanDaPalavra.textContent = arrayDasLetras.slice(0, j).join("");
+    if (j > 0) {
+      cursor.classList.remove(piscando);
+      apagarPalavra();
+    } else {
+      i++
+      if(i >= arrayDasPalavras.length) i = 0;
+      cursor.classList.add(piscando);
+      setTimeout(escreverPalavra, 750);
+    }
+  }, 60);
+}
+
+cursor.classList.add(piscando);
+escreverPalavra();
